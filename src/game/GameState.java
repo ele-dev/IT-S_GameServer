b4cfe793f97459dcd2cda1a75df4a9e69acd3b73
Elias Geiger <elias@ele-dev.de>
2020-09-25 20:23:48 +0200

    Added Game Over status flag with setter and getters

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -2,9 +2,12 @@ package game;
 
 public class GameState {
 
+	// static class members //
 	private static String actingTeam = "cross";
 	private static byte[][] gameField = new byte[3][3];
+	private static boolean gameIsOver = false;
 	
+	// Method clear the game field
 	public static void clearField() {
 		for(int i = 0; i < 3; i++)
 		{
@@ -15,6 +18,7 @@ public class GameState {
 		}
 	}
 	
+	// Method switches the team that is currently allowed to make a move
 	public static void switchActingTeam() {
 		if(actingTeam.equalsIgnoreCase("cross")) {
 			actingTeam = "circle";
@@ -23,15 +27,28 @@ public class GameState {
 		}
 	}
 	
+	// Return the acting team
 	public static String getActingTeam() {
 		return actingTeam;
 	}
 	
+	// Method for overwritig the current game field state with a new/updated one
 	public static void updateField(byte[][] newField) {
 		gameField = newField;
 	}
 	
+	// Return the current state of the game field
 	public static byte[][] getCurrentFieldState() {
 		return gameField;
+	}
+	
+	// Method for marking the game to be over
+	public static void setGameOver() {
+		gameIsOver = true;
+	}
+	
+	// Return the game over status
+	public static boolean isGameOver() {
+		return gameIsOver;
 	}
 }
