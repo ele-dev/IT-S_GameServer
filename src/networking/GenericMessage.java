bbb77a3013fd06c81153b34d1c78eba8ddfb4dc6
Elias Geiger <elias@ele-dev.de>
2020-10-24 11:02:49 +0200

    Added new signal message type MSG_FOUND_MATCH

--- src/networking/GenericMessage.java
+++ src/networking/GenericMessage.java
@@ -34,6 +34,7 @@ public abstract class GenericMessage implements Serializable {
 	public static final int MSG_ACCOUNT_STATS = 3005;
 	public static final int MSG_JOIN_QUICKMATCH = 3010;
 	public static final int MSG_ABORT_MATCH_SEARCH = 3011;
+	public static final int MSG_FOUND_MATCH = 3012;
 	public static final int MSG_LEAVE_GAME = 3015;
 	
 	
